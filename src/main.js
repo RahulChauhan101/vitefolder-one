d58eb88d0b0dd808b8e268a61e07329b5f4df276
RahulChauhan101 <rahul.chauhan12241@gmail.com>
2025-06-14 12:35:05 +0530

    main.js line no 7

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+  
+
+// main.js
+
 
 document.querySelector('#app').innerHTML = `
   <div>
